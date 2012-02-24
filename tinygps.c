e813aa3f2ff015b8998c75f5ed86252f5e1faf80
Stefan Tomanek <stefan@pico.ruhr.de>
2012-02-24 16:20:22 +0100

    add I2C code to main program

--- tinygps.c
+++ tinygps.c
@@ -5,11 +5,17 @@
 #include <util/delay.h>
 
 #include "nmea.h"
+#include "usiTwiSlave.h"
+
+#define TWIADDRESS 0x11
 
 extern struct nmea_rmc_t nmea_rmc;
 
 int main(void) {
 	DDRD |= 1<<PD5;
+
+	usiTwiSlaveInit(TWIADDRESS);
+	usiTwiSetTransmitWindow( &nmea_rmc, sizeof(struct nmea_rmc_t) );
 
 	char *input = "$GPRMC,125934.000,V,5123.1457,N,00645.0808,E,,,020811,,*13\r\n";
 	while (input[0]) {
