ee60c5c3da13151ac5b4cc8d21a29bfe1ad8bef1
Stefan Tomanek <stefan@pico.ruhr.de>
2012-04-13 23:12:26 +0200

    move public structs to nmea_structs.h

--- nmea.h
+++ nmea.h
@@ -1,35 +1,4 @@
-#define NMEA_MINUTE_FRACTS 4
-#define NMEA_ALTITUDE_FRACTS 2
-
-#define NMEA_RMC_FLAGS_STATUS_OK 0
-#define NMEA_RMC_FLAGS_LAT_NORTH 1
-#define NMEA_RMC_FLAGS_LON_EAST 2
-
-struct coord {
-	/* degrees, 0-180 or 0-90 */
-	uint8_t deg;
-	/* minutes, 0-60 */
-	uint8_t min;
-	/* fractions of minutes saved as BCD */
-	uint8_t frac[(NMEA_MINUTE_FRACTS+1)/2];
-};
-
-struct altitude_t {
-	int16_t m;
-	uint8_t frac[(NMEA_ALTITUDE_FRACTS+1)/2];
-};
-
-struct clock_t {
-	uint8_t hour;
-	uint8_t minute;
-	uint8_t second;
-};
-
-struct date_t {
-	uint8_t day;
-	uint8_t month;
-	uint8_t year;
-};
+#include "nmea_structs.h"
 
 struct nmea_rmc_t {
 	/* flag bits (lsb to msb):
@@ -45,17 +14,6 @@ struct nmea_rmc_t {
 };
 
 struct nmea_gga_t {
-	struct altitude_t alt;
-	uint8_t quality;
-	uint8_t sats;
-};
-
-struct nmea_data_t {
-	uint8_t flags;
-	struct date_t date;
-	struct clock_t clock;
-	struct coord lat;
-	struct coord lon;
 	struct altitude_t alt;
 	uint8_t quality;
 	uint8_t sats;
