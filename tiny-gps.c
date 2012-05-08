912587188fce96daeb62fac5d76f2e3d94a58b58
Stefan Tomanek <stefan@pico.ruhr.de>
2012-05-09 01:12:22 +0200

    use RX interrupt and ring buffer for GPS

--- tiny-gps.c
+++ tiny-gps.c
@@ -11,10 +11,17 @@
 
 #include <util/setbaud.h>
 
+#define RX_BUF_SIZE 4
+static volatile char rx_buf[RX_BUF_SIZE];
+static volatile uint8_t rx_buf_r;
+static volatile uint8_t rx_buf_w;
+
 extern struct nmea_data_t nmea_data;
 int main(void) {
 	DDRD = (1<<PD5);
 	UCSRB = (1<<RXEN);
+
+	UCSRB = (1<<RXEN | 1<<RXCIE);
 	UCSRC = (0<<USBS)|(3<<UCSZ0);
 
 	UCSRA = (USE_2X<<U2X);
@@ -23,14 +30,21 @@ int main(void) {
 	usiTwiSlaveInit(TWIADDRESS);
 	usiTwiSetTransmitWindow( &nmea_data, sizeof(struct nmea_data_t) );
 
+	rx_buf_r = 0;
+	rx_buf_w = 0;
+
 	PORTD &= ~(1<<PD5);
 
 	sei();
 	while (1) {
 		/* read from the serial UART */
-		while(!(UCSRA & (1<<RXC))) {}
-		char c = UDR;
-		nmea_process_character(c);
+		while (rx_buf_w != rx_buf_r) {
+			nmea_process_character(rx_buf[rx_buf_r]);
+			rx_buf_r++;
+			if (rx_buf_r >= RX_BUF_SIZE) {
+				rx_buf_r = 0;
+			}
+		}
 
 		/* toggle gps fix indicator */
 		if (nmea_data.flags & 1<<NMEA_RMC_FLAGS_STATUS_OK) {
@@ -38,5 +52,13 @@ int main(void) {
 		} else {
 			PORTD &= ~(1<<PD5);
 		}
+	}
+}
+
+ISR(USART_RX_vect) {
+	rx_buf[rx_buf_w] = UDR;
+	rx_buf_w++;
+	if (rx_buf_w >= RX_BUF_SIZE) {
+		rx_buf_w = 0;
 	}
 }
