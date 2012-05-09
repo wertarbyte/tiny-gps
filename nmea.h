86367002914f7ec237c09847fd3b7ec6eeaa9aa2
Stefan Tomanek <stefan@pico.ruhr.de>
2012-05-09 21:19:55 +0200

    remove unneeded members from internal structs

--- nmea.h
+++ nmea.h
@@ -7,18 +7,24 @@ struct nmea_rmc_t {
 	 * 2 longitude alignment (1 == E, 0 == W)
 	 */
 	uint8_t flags;
+#if PARSE_GPS_TIME
 	struct date_t date;
 	struct clock_t clock;
+#endif
 	struct coord lat;
 	struct coord lon;
 };
 
 struct nmea_gga_t {
 	uint8_t flags;
+#if PARSE_GPS_TIME
 	struct clock_t clock;
+#endif
 	struct coord lat;
 	struct coord lon;
+#if PARSE_GPS_ALTITUDE
 	struct altitude_t alt;
+#endif
 	uint8_t quality;
 	uint8_t sats;
 };
